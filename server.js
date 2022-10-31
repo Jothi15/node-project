3230f30fe1de1760dbc27112d30d24bd52ded5b7
Ashfaq <tricksfree.in@gmail.com>
2022-10-31 15:45:07 +0530

    hi

--- server.js
+++ server.js
@@ -7,44 +7,24 @@ const cors = require('cors');
 const Employe = require("./Modules/employees")
 const multer = require("multer")
 
-// const passport = require("passport");
-// const fresher = require("./routes/api/fresher");
-// const users = require("./routes/api/users");
-// const company = require("./routes/api/company");
-// const cors = require("cors");
-// const path = require("path");
 
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json())
 
-// app.use(cors());
-
-// Bodyparser middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
+ 
 app.use(bodyParser.json());
 
-// --DB Config
-// const db = require("./config/keys").mongoURI;
-// module.exports = {
-//     mongoURI: "mongodb://localhost/bril",
-//     secretOrKey: "secret"
-//   };
-
+ 
 mongoose
   .connect("mongodb+srv://jothi:jothi@cluster0.uhnsitc.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
+    
   })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
 
-// --Passport middleware
-// app.use(passport.initialize());
-  
-// --Passport config
-// require("./config/passport")(passport);
+ 
 
 // --Routes
 app.use("/", employe);
@@ -52,17 +32,7 @@ app.use("/a", employes);
 
 
 
-// app.use("/api/fresher", fresher);
-// app.use("/api/company", company);
-// app.use(express.static(path.join(__dirname, "client/build")));
-// if (process.env.NODE_ENV === "production") {
-//   console.log(process.env.NODE_ENV);
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
-//   });
-// }
-
+ 
 global.__basedir = __dirname;
 
 // const corsOptions = {
